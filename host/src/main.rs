15ee07ce8049605a5db528c5a2be7dd592f65f95
hzysvilla <ecjgvmhc@gmail.com>
2025-01-13 12:20:27 +0000

    intrduoce rize-zero

--- host/src/main.rs
+++ host/src/main.rs
@@ -24,7 +24,8 @@ fn main() {
     // ExecutorEnvBuilder::build().
 
     // For example:
-    let input: u32 = 15 * u32::pow(2, 27) + 1;
+    // let input: u32 = 15 * u32::pow(2, 27) + 1;
+    let input: u32 = 1000;
     let env = ExecutorEnv::builder()
         .write(&input)
         .unwrap()
